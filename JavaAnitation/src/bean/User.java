8cf395838b3aa14f08125850170612c9edb5f405
王立 <wangli@ruulai.com>
2016-10-13 17:33:46 +0800

    添加Java版编译时运行apt

---
+++ JavaAnitation/src/bean/User.java
@@ -0,0 +1,20 @@
+package bean;
+
+import com.zhy.annotationprocess.annotation.Seriable;
+
+
+public class User {
+	    
+	@Seriable
+	private  String username;  
+	@Seriable
+	private  String password;  
+	@Seriable
+	private  String three;  
+	@Seriable
+	private  String four;  
+	    
+	    
+	    
+	    
+}
