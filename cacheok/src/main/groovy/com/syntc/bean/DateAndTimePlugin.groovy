a07e6706ac41c2fd19f12b44a8a765e0b0628a51
wangli <352085768@qq.com>
2016-10-25 14:08:51 +0800

    update plugin & Task

---
+++ cacheok/src/main/groovy/com/syntc/bean/DateAndTimePlugin.groovy
@@ -0,0 +1,7 @@
+package com.syntc.bean;
+
+class DateAndTimePluginExtension {
+
+    String timeFormat = "MM/dd/yyyyHH:mm:ss.SSS"
+    String dateFormat = "yyyy-MM-dd"
+}
--- cacheok/src/main/groovy/com/syntc/plugin/myplugin.groovy
+++ cacheok/src/main/groovy/com/syntc/plugin/myplugin.groovy
@@ -1,4 +1,6 @@
-package com.syntc.plugin;
+package com.syntc.plugin
+
+import com.syntc.bean.DateAndTimePluginExtension;
 
 // 注意com.abc.xiaoming.properties  这个文件的命名，你可以随意取名(和包名无关)，但是后面使用这个插件的时候(其他module)，会用到这个名字。比如，你取名为com.abc.xiaoming.properties ，而在其他build.gradle文件中使用自定义的插件时候则需写成：
 // apply plugin: 'com.abc.xiaoming'
@@ -9,10 +11,32 @@ import org.gradle.api.Project
 
 public class MyPlugin implements Plugin<Project> {
 
+    @Override
     void apply(Project project) {
-        System.out.println("========================");
-        System.out.println("hello gradle plugin!");
-        System.out.println("========================");
+
+            project.extensions.create("dateAndTime", DateAndTimePluginExtension)
+
+
+            System.out.println("========================");
+            System.out.println("hello gradle plugin!");
+            System.out.println("========================");
+
+
+        //创建两个Task
+
+            project.task('ashowTime') << {
+                println "Current time is " + new Date().format(project.dateAndTime.timeFormat)
+            }
+
+            project.tasks.create('ashowDate') << {
+                println "Current date is " + new Date().format(project.dateAndTime.dateFormat)
+            }
+
+
+
+
+
+
     }
 }
 
